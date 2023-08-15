0365b04e22c6b1bf36be0a9a0441a874b7f9ab81
VickyJana <mail2janavicky@gmail.com>
2023-08-15 18:59:38 +0530

    API RESTASSURED FRAMEWORK

---
+++ src/main/java/bdd/APIFramework/App.java
@@ -0,0 +1,17 @@
+package bdd.APIFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+
+	    public static void main(String args[]) {
+
+	        String current = System.getProperty("user.dir");
+	        System.out.println("Current working directory in Java : " + current);
+	    }
+	}
+
